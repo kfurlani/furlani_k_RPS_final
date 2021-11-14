9bae4e71db2d99eff848959b1668ca3f8444d5d9
Krystal Furlani <krystal@Krystals-MacBook-Pro.local>
2021-11-13 19:19:40 -0500

    added result function and title

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint
-from gameComponents import winLose, gameVars
+from gameComponents import winLose, gameVars, gameResult
 
+print("***ROCK***PAPER***SCISSORS***")
 
-# set up our game loop so that we can keep playing and not exit
 while gameVars.player is False:
     gameVars.player = input("Choose your weapon: rock, paper or scissors: ")
     gameVars.computer = gameVars.choices[randint(0, 2)]
@@ -10,41 +10,15 @@ while gameVars.player is False:
     print("player chose: " + gameVars.player)
     print("computer chose: " + gameVars.computer)
 
-    if gameVars.computer == gameVars.player:
-        # tie - nothing else to compare, so it'll exit
-        print("tie! try again")
-
-    elif gameVars.player == "rock":
-        if gameVars.computer == "paper":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
-
-    elif gameVars.player == "paper":
-        if gameVars.computer == "scissors":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
-
-    elif gameVars.player == "scissors":
-        if gameVars.computer == "rock":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
+    gameResult.winner()
 
     print("player life count: " + str(gameVars.playerLives))
     print("computer life count: " + str(gameVars.computerLives))
 
     if gameVars.playerLives == 0:
-        winLose.winorlose("lost")
+        winLose.winorlose("lost </3")
 
     elif gameVars.computerLives == 0:
-        winLose.winorlose("won")
+        winLose.winorlose("won <3333")
 
     gameVars.player = False
